2ae37b04678eb4085e588b5a67a12bbe9e146661
xhy-ml <76425500+xhy-ml@users.noreply.github.com>
2022-12-05 19:50:11 +0800

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -23,6 +23,10 @@ def get_net(input_depth, NET_TYPE, pad, upsample_mode, n_channels=3, act_fun='Le
         net = UNet(num_input_channels=input_depth, num_output_channels=3, 
                    feature_scale=4, more_layers=0, concat_x=False,
                    upsample_mode=upsample_mode, pad=pad, norm_layer=nn.BatchNorm2d, need_sigmoid=True, need_bias=True)
+    elif NET_TYPE =='UNet_Nested':
+        net = UNet++(num_input_channels=input_depth, num_output_channels=3, 
+                   feature_scale=4, more_layers=0, concat_x=False,
+                   upsample_mode=upsample_mode, pad=pad, norm_layer=nn.BatchNorm2d, need_sigmoid=True, need_bias=True) 
     elif NET_TYPE == 'identity':
         assert input_depth == 3
         net = nn.Sequential()
